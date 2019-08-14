70928d9713d22d6a30a92fc18472f9748d24f324
mitanshu001 <mitanshu.sheo@iitg.ernet.in>
2019-08-14 17:03:08 +0530

    votetry updated

--- webapp/votetry.js
+++ webapp/votetry.js
@@ -2,15 +2,21 @@ Web3 = require('web3');
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 var account;
 web3.eth.getAccounts().then((f) => {
- account = f[1];
- console.log(account)
+    account = f[0];
+    console.log(account);
+    const fs = require('fs');
+    let rawdata = fs.readFileSync('/home/mitanshu/projects/ElectionVoting/build/contracts/Voting.json');
+    let votingJson = JSON.parse(rawdata);
+    var abi = votingJson.abi;
+    contract = new web3.eth.Contract(abi);
+    contract.options.address = "0xf7b4876Fc97f53868F1982336998a1341d82b65C";
+    contract.methods.addCandidate("Guwahati","sfdsdfs",16012454530).send({from:account}).then((f) => {
+        console.log(f);
+    })
+    
 })
 
-var text = '{ "name":"John", "birth":"1986-12-14", "city":"New York"}';
+// contract.methods.candidateCount().call().then((f) => {
+//     console.log(f);
+// })
 
-var obj = JSON.parse(text);
-
-var request = new XMLHttpRequest();
-request.open("GET", "build/contracts/Voting.json", false);
-request.send(null)
-var my_JSON_object = JSON.parse(request.responseText);
