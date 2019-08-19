907c6848984abab83c9ba74921a81b46d0d440fb
ashishranjan2011 <ashishranjan@iitg.ac.in>
2019-08-19 18:16:12 +0530

    results updated

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,6 +1,7 @@
-    pragma solidity ^0.5.0;
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
-    contract Voting{
+contract Voting{
     // Candidate
     struct Person{
         string name;
@@ -47,9 +48,6 @@
         bool doesExist;
     }
 
-    struct Results{
-        mapping (uint => Candidate[]) constituencyIdtoSortedList;
-    }
 
     mapping(uint => Candidate) candidates;
     mapping(uint => Officer) officersList; // aadhar
@@ -58,7 +56,8 @@
     mapping(uint => Booth) public boothList;
     mapping(string => uint)  constituencyNameToId;
     mapping(address => uint) public machineToBooth;
-    mapping(uint => uint[]) public constituencyToBooth;
+    mapping(uint => Booth[]) public constituencyToBooth;
+    mapping(uint => Constituency) public pinToConstituency;
 
     enum StateType { PreVoting, Voting, Result}
 
@@ -76,15 +75,16 @@
     {
         addConstituency("Guwahati");
         addBooth("GS-Road",1);
-        addOfficer("Ashish",12345678,1);
+        addOfficer("Rahul Kumar Gupta",910611041461,1);
         State = StateType.PreVoting;
+        pinToConstituency[456789] = constituencyList[1];
     }
 
     function addBooth(string memory boothAddress,uint constituencyId)public {
         boothCount++;
         boothList[boothCount] = Booth(boothAddress,boothCount,constituencyId,true);
         constituencyList[constituencyId].boothCount++;
-        constituencyToBooth[constituencyId].push(boothCount);
+        constituencyToBooth[constituencyId].push(boothList[boothCount]);
     }
 
     function addConstituency(string memory name) public {
@@ -174,157 +174,180 @@
         State = StateType.Result;
     }
 
-    function results(uint constituencyId) public view returns (uint){
-        uint maxVotes = 0;
-        uint maxId;
-        for(uint i = 1; i<=candidateCount; i++){
-            if(candidates[i].constituencyId == constituencyId && maxVotes <= candidates[i].voteCount){
-                maxId = candidates[i].id;
+
+
+        // mapping (uint => Candidate[]) result;
+        mapping (uint => Candidate[]) constituencyToCandidate;
+        // Candidate[] result;
+
+    event Result(
+        Candidate[] result
+    );
+
+    function getResults(uint constituencyId) public {
+        require(State == StateType.Result,"Invalid State");
+        require(constituencyId > 0 && constituencyId <= constituencyCount,"Invalid Constituency");
+            uint size = constituencyToCandidate[constituencyId].length;
+            uint count = 0 ;
+            while(size > 1 ){
+                uint maxVotes=0;
+                uint maxId = 0;
+                for(uint j=0;j<size-count;++j){
+                    if(maxVotes <= constituencyToCandidate[constituencyId][j].voteCount){
+                        maxVotes = constituencyToCandidate[constituencyId][j].voteCount;
+                        maxId = j;
+                    }
+                }
+                Candidate memory temp = constituencyToCandidate[constituencyId][count];
+                constituencyToCandidate[constituencyId][count] = constituencyToCandidate[constituencyId][maxId];
+                constituencyToCandidate[constituencyId][maxId] = temp;
+                count++;
             }
-        }
-        return maxId;
+        
+        emit Result(constituencyToCandidate[constituencyId]);
     }
-    }
+}
 
 
-    // contract Election {
-    //     // Model a Candidate
-    //     struct Candidate {
-    //         uint id;
-    //         string name;
-    //         uint voteCount;
-    //     }
 
-    //     // Store accounts that have voted
-    //     mapping(address => bool) public voters;
-    //     // Store Candidates
-    //     // Fetch Candidate
-    //     mapping(uint => Candidate) public candidates;
-    //     // Store Candidates Count
-    //     uint public candidatesCount;
+// contract Election {
+//     // Model a Candidate
+//     struct Candidate {
+//         uint id;
+//         string name;
+//         uint voteCount;
+//     }
 
-    //     // voted event
-    //     event votedEvent (
-    //         uint indexed _candidateId
-    //     );
+//     // Store accounts that have voted
+//     mapping(address => bool) public voters;
+//     // Store Candidates
+//     // Fetch Candidate
+//     mapping(uint => Candidate) public candidates;
+//     // Store Candidates Count
+//     uint public candidatesCount;
 
-    //     constructor() public {
-    //         addCandidate("Candidate 1");
-    //         addCandidate("Candidate 2");
-    //     }
+//     // voted event
+//     event votedEvent (
+//         uint indexed _candidateId
+//     );
 
-    //     function addCandidate (string memory _name) private {
-    //         candidatesCount ++;
-    //         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-    //     }
+//     constructor() public {
+//         addCandidate("Candidate 1");
+//         addCandidate("Candidate 2");
+//     }
 
-    //     function vote (uint _candidateId) public {
-    //         // require that they haven't voted before
-    //         require(!voters[msg.sender],"You have already voted!");
+//     function addCandidate (string memory _name) private {
+//         candidatesCount ++;
+//         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+//     }
 
-    //         // require a valid candidate
-    //         require(_candidateId > 0 && _candidateId <= candidatesCount,"Candidate is not valid!");
+//     function vote (uint _candidateId) public {
+//         // require that they haven't voted before
+//         require(!voters[msg.sender],"You have already voted!");
 
-    //         // record that voter has voted
-    //         voters[msg.sender] = true;
+//         // require a valid candidate
+//         require(_candidateId > 0 && _candidateId <= candidatesCount,"Candidate is not valid!");
 
-    //         // update candidate vote Count
-    //         candidates[_candidateId].voteCount ++;
+//         // record that voter has voted
+//         voters[msg.sender] = true;
 
-    //         // trigger voted event
-    //         emit votedEvent(_candidateId);
-    //     }
-    // }
+//         // update candidate vote Count
+//         candidates[_candidateId].voteCount ++;
 
-
-    // contract Voting {
-    //     // an event that is called whenever a Candidate is added so the frontend could
-    //     // appropriately display the candidate with the right element id (it is used
-    //     // to vote for the candidate, since it is one of arguments for the function "vote")
-    //     event AddedCandidate(uint candidateID);
-
-    //     // describes a Voter, which has an id and the ID of the candidate they voted for
-    //     address owner;
-    //     function Voting()public {
-    //         owner=msg.sender;
-    //     }
-    //     modifier onlyOwner {
-    //         require(msg.sender == owner);
-    //         _;
-    //     }
-    //     struct Voter {
-    //         bytes32 uid; // bytes32 type are basically strings
-    //         uint candidateIDVote;
-    //     }
-    //     // describes a Candidate
-    //     struct Candidate {
-    //         bytes32 name;
-    //         bytes32 party;
-    //         // "bool doesExist" is to check if this Struct exists
-    //         // This is so we can keep track of the candidates
-    //         bool doesExist;
-    //     }
-
-    //     // These state variables are used keep track of the number of Candidates/Voters
-    //     // and used to as a way to index them    
-    //     uint numCandidates; // declares a state variable - number Of Candidates
-    //     uint numVoters;
+//         // trigger voted event
+//         emit votedEvent(_candidateId);
+//     }
+// }
 
 
-    //     // Think of these as a hash table, with the key as a uint and value of
-    //     // the struct Candidate/Voter. These mappings will be used in the majority
-    //     // of our transactions/calls
-    //     // These mappings will hold all the candidates and Voters respectively
-    //     mapping (uint => Candidate) candidates;
-    //     mapping (uint => Voter) voters;
+// contract Voting {
+//     // an event that is called whenever a Candidate is added so the frontend could
+//     // appropriately display the candidate with the right element id (it is used
+//     // to vote for the candidate, since it is one of arguments for the function "vote")
+//     event AddedCandidate(uint candidateID);
 
-    //     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    //      *  These functions perform transactions, editing the mappings *
-    //      * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//     // describes a Voter, which has an id and the ID of the candidate they voted for
+//     address owner;
+//     function Voting()public {
+//         owner=msg.sender;
+//     }
+//     modifier onlyOwner {
+//         require(msg.sender == owner);
+//         _;
+//     }
+//     struct Voter {
+//         bytes32 uid; // bytes32 type are basically strings
+//         uint candidateIDVote;
+//     }
+//     // describes a Candidate
+//     struct Candidate {
+//         bytes32 name;
+//         bytes32 party;
+//         // "bool doesExist" is to check if this Struct exists
+//         // This is so we can keep track of the candidates
+//         bool doesExist;
+//     }
 
-    //     function addCandidate(bytes32 name, bytes32 party) onlyOwner public {
-    //         // candidateID is the return variable
-    //         uint candidateID = numCandidates++;
-    //         // Create new Candidate Struct with name and saves it to storage.
-    //         candidates[candidateID] = Candidate(name,party,true);
-    //         AddedCandidate(candidateID);
-    //     }
-
-    //     function vote(bytes32 uid, uint candidateID) public {
-    //         // checks if the struct exists for that candidate
-    //         if (candidates[candidateID].doesExist == true) {
-    //             uint voterID = numVoters++; //voterID is the return variable
-    //             voters[voterID] = Voter(uid,candidateID);
-    //         }
-    //     }
-
-    //     /* * * * * * * * * * * * * * * * * * * * * * * * * *
-    //      *  Getter Functions, marked by the key word "view" *
-    //      * * * * * * * * * * * * * * * * * * * * * * * * * */
+//     // These state variables are used keep track of the number of Candidates/Voters
+//     // and used to as a way to index them    
+//     uint numCandidates; // declares a state variable - number Of Candidates
+//     uint numVoters;
 
 
-    //     // finds the total amount of votes for a specific candidate by looping
-    //     // through voters
-    //     function totalVotes(uint candidateID) view public returns (uint) {
-    //         uint numOfVotes = 0; // we will return this
-    //         for (uint i = 0; i < numVoters; i++) {
-    //             // if the voter votes for this specific candidate, we increment the number
-    //             if (voters[i].candidateIDVote == candidateID) {
-    //                 numOfVotes++;
-    //             }
-    //         }
-    //         return numOfVotes;
-    //     }
+//     // Think of these as a hash table, with the key as a uint and value of
+//     // the struct Candidate/Voter. These mappings will be used in the majority
+//     // of our transactions/calls
+//     // These mappings will hold all the candidates and Voters respectively
+//     mapping (uint => Candidate) candidates;
+//     mapping (uint => Voter) voters;
 
-    //     function getNumOfCandidates() public view returns(uint) {
-    //         return numCandidates;
-    //     }
+//     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//      *  These functions perform transactions, editing the mappings *
+//      * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-    //     function getNumOfVoters() public view returns(uint) {
-    //         return numVoters;
-    //     }
-    //     // returns candidate information, including its ID, name, and party
-    //     function getCandidate(uint candidateID) public view returns (uint,bytes32, bytes32) {
-    //         return (candidateID,candidates[candidateID].name,candidates[candidateID].party);
-    //     }
-    // }
+//     function addCandidate(bytes32 name, bytes32 party) onlyOwner public {
+//         // candidateID is the return variable
+//         uint candidateID = numCandidates++;
+//         // Create new Candidate Struct with name and saves it to storage.
+//         candidates[candidateID] = Candidate(name,party,true);
+//         AddedCandidate(candidateID);
+//     }
+
+//     function vote(bytes32 uid, uint candidateID) public {
+//         // checks if the struct exists for that candidate
+//         if (candidates[candidateID].doesExist == true) {
+//             uint voterID = numVoters++; //voterID is the return variable
+//             voters[voterID] = Voter(uid,candidateID);
+//         }
+//     }
+
+//     /* * * * * * * * * * * * * * * * * * * * * * * * * *
+//      *  Getter Functions, marked by the key word "view" *
+//      * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+//     // finds the total amount of votes for a specific candidate by looping
+//     // through voters
+//     function totalVotes(uint candidateID) view public returns (uint) {
+//         uint numOfVotes = 0; // we will return this
+//         for (uint i = 0; i < numVoters; i++) {
+//             // if the voter votes for this specific candidate, we increment the number
+//             if (voters[i].candidateIDVote == candidateID) {
+//                 numOfVotes++;
+//             }
+//         }
+//         return numOfVotes;
+//     }
+
+//     function getNumOfCandidates() public view returns(uint) {
+//         return numCandidates;
+//     }
+
+//     function getNumOfVoters() public view returns(uint) {
+//         return numVoters;
+//     }
+//     // returns candidate information, including its ID, name, and party
+//     function getCandidate(uint candidateID) public view returns (uint,bytes32, bytes32) {
+//         return (candidateID,candidates[candidateID].name,candidates[candidateID].party);
+//     }
+// }
